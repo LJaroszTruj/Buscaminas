b1a3103b24f89d4767f7baabe36608ddaceb7362
Luis Jarosz <luisito.lj@gmail.com>
2013-04-25 13:44:25 +0200

    Filas/Columnas
    Mirar cuál es el fallo...

--- Buscaminas/Buscaminas/Form1.cs
+++ Buscaminas/Buscaminas/Form1.cs
@@ -19,32 +19,56 @@ namespace Buscaminas
     {
         //declaro el array de botones
         Button[,] matrizBotones;
-        int filas = 15;
+        int filas = 20;
         int columnas = 20;
+        int minas = 20;
         int anchoBoton = 20;
+
+
+        //Si el Tag es 1 es que no hay bomba
+        //Si el Tag es 2 es que hay bomba
 
         public Form1()
         {
             InitializeComponent();
             
 
+            matrizBotones = new Button[filas, columnas];
+
             this.Height = columnas * anchoBoton + 40;
             this.Width = filas * anchoBoton + 20;
-
-            matrizBotones = new Button[filas, columnas];
             
-            for (int i = 0; i < filas; i++)
-                for (int j = 0; j < columnas; j++)
-                {
+            for (int i = 0; i < filas; i++){
+                for (int j = 0; j < columnas; j++){
                     Button boton = new Button();
                     //boton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
                     boton.Width = anchoBoton;
                     boton.Height = anchoBoton;
                     boton.Location = new Point(i * anchoBoton, j * anchoBoton);
                     boton.Click += chequeaBoton;
-                    matrizBotones[i, j] = boton;
+                    boton.Tag = "1";
+                    matrizBotones[j, i] = boton;
                     panel1.Controls.Add(boton);
+
+                    }
+                poneMinas();
                 }
+        }
+
+        private void poneMinas(){
+            Random aleatorio = new Random();
+            int x = 0, y = 0;
+            for (int i = 0; i < minas; i++){
+                x = aleatorio.Next(filas);
+                y = aleatorio.Next(columnas);
+
+                    while(!matrizBotones[y, x].Tag.Equals("1")) {
+                        x = aleatorio.Next(filas);
+                        y = aleatorio.Next(columnas);              
+                    }
+                    matrizBotones[y, x].Tag = "2";
+                    matrizBotones[y, x].Text = "B";
+            }
         }
 
         private void chequeaBoton(object sender, EventArgs e){
@@ -52,9 +76,17 @@ namespace Buscaminas
             int columna = b.Location.X / anchoBoton;
             int fila = b.Location.Y / anchoBoton;
             //De esta manera sólo se consigen botones de 3x3
-            for (int i = 12; i < filas; i++){
-              for (int j = 17; j < columnas; j++){
-                   matrizBotones[fila, columna].BackColor = Color.Blue;
+            
+            for (int i = -1; i < 2; i++){
+              for (int j = -1; j < 2; j++){
+                  if ((columna + j < columnas) &&(columna + j >= 0)
+                     &&(fila + i < filas) && (fila + i >= 0)){
+                         if (matrizBotones[columna + j, fila + i].BackColor != Color.Tomato){
+
+                             matrizBotones[columna + j, fila + i].BackColor = Color.Tomato;
+                             chequeaBoton(matrizBotones[columna + j, fila + i], e);
+                       }
+                   }
                 }
             }
         }
